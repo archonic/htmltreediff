9e2597d3e8b489e7232523a38cb1af9f420220f4
Joshua Flark <joshuajmark@gmail.com>
2019-11-25 13:34:56 -0800

    tmp path

--- htmltreediff/cli.py
+++ htmltreediff/cli.py
@@ -9,7 +9,7 @@ def main(argv=None):
         html_a = file_a.read()
     with open(argv[2]) as file_b:
         html_b = file_b.read()
-    output_filename = f"output_{secrets.token_urlsafe(6)}.html"
+    output_filename = f"tmp/doc_diff/output_{secrets.token_urlsafe(6)}.html"
     f = open(output_filename, "w")
     f.write(diff(html_a, html_b, cutoff=0.0, pretty=True))
     f.close()
